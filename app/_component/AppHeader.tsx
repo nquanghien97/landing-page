9495eeaccd6b968f4fafd6ef551bdf28eb609fde
nquanghien97 <nquanghien97@gmail.com>
2024-08-12 01:03:27 +0700

    responsive header

--- app/_component/AppHeader.tsx
+++ app/_component/AppHeader.tsx
@@ -31,100 +31,104 @@ function AppHeader() {
 
   return (
     <>
-      <div className={`h-[70px] fixed inset-x-0 top-0 flex items-center justify-center px-4 duration-300 z-50 bg-black text-white ${isSticky ? 'is-sticky' : ''}`}>
-        <p className="max-md:w-full max-md:text-center">Logo</p>
-        <div className='flex gap-2'>
-          <div className="flex items-center gap-2">
-            <ul className="flex items-center gap-6 max-md:flex-col max-md:hidden">
-              {listHeader.map(item => (
-                <li key={item.path} className="px-3 py-1">
-                  {item.children ? (
-                    <MenuDropdown
-                      title={item.title}
-                      path={item.path}
-                    >
-                      <ul className="bg-[#0f0f10] flex flex-col min-w-[15rem] border border-[#ffffff12] rounded-md md:py-2 text-white">
-                        {item.children.map(childItem => (
-                          <li key={childItem.path}>
-                            <Link className={`cursor-pointer hover:text-[#ff9900] px-4 py-2 w-full flex items-center`} href={childItem.path}>
-                              {childItem.title}
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    </MenuDropdown>
-                  ) : (
-                    <Link href={item.path} className={`hover:text-[#ff9900] duration-300 ${pathname === item.path ? 'text-[#ff9900]' : ''}`}>
-                      {item.title}
-                    </Link>
-                  )}
-                </li>
-              ))}
-            </ul>
-            <div className="flex items-center gap-4">
-              <div className="max-md:hidden">
-                <BaseInput placeholder='Tìm kiếm...' startIcon={<SearchIcon />} />
-              </div>
-              <div className='cursor-pointer'>
-                <ShoppingCartIcon color='white' />
-              </div>
-            </div>
+      <div className={`h-[70px] fixed inset-x-0 top-0 px-4 duration-300 z-50 bg-black text-white ${isSticky ? 'is-sticky' : ''}`}>
+        <div className="max-w-6xl m-auto flex items-center justify-between h-full">
+          <div className="px-2 max-lg:flex-1">
+            <p className="max-lg:w-full max-lg:text-center">Logo</p>
           </div>
-          {!isOpenSidebar && (
-            <div className="cursor-pointer md:hidden p-2 hover:bg-[#ccc] hover:opacity-60 rounded-full duration-300" onClick={() => setIsOpenSidebar(true)}>
-              <MenuIcon fill="white" />
-            </div>
-          )}
-          <AppSidebar
-            open={isOpenSidebar}
-            setOpen={setIsOpenSidebar}
-            start="-24rem"
-            end="0"
-            exit="-24rem"
-          >
-            <div  className="py-7">
-              <div>
-                <BaseInput placeholder='Tìm kiếm...' endIcon={<SearchIcon />} />
-              </div>
-              <ul className="flex max-md:flex-col ">
-                <li className="flex px-3 py-1 hover:bg-[#0000000d] text-black">
-                  <Link href="/" className="p-3 w-full h-full">
-                    Trang chủ
-                  </Link>
-                </li>
-                <li className="flex px-3 py-1 hover:bg-[#0000000d] text-black">
-                  <Link href="/" className="p-3 w-full h-full">
-                    Giới thiệu
-                  </Link>
-                </li>
-                <li className="flex px-3 py-1 hover:bg-[#0000000d] text-black">
-                  <Link href="/" className="p-3 w-full h-full">
-                    Dịch vụ
-                  </Link>
-                </li>
-                <li className="flex px-3 py-1 hover:bg-[#0000000d] text-black">
-                  <Link href="/" className="p-3 w-full h-full">
-                    Kiến trúc
-                  </Link>
-                </li>
-                <li className="flex px-3 py-1 hover:bg-[#0000000d] text-black">
-                  <Link href="/" className="p-3 w-full h-full">
-                    Nội thất
-                  </Link>
-                </li>
-                <li className="flex px-3 py-1 hover:bg-[#0000000d] text-black">
-                  <Link href="/" className="p-3 w-full h-full">
-                    Tin tức
-                  </Link>
-                </li>
-                <li className="flex px-3 py-1 hover:bg-[#0000000d] text-black">
-                  <Link href="/" className="p-3 w-full h-full">
-                    Liên hệ
-                  </Link>
-                </li>
+          <div className='flex gap-2'>
+            <div className="flex items-center gap-2">
+              <ul className="flex items-center gap-4 max-lg:flex-col max-lg:hidden">
+                {listHeader.map(item => (
+                  <li key={item.path} className="px-3 py-1">
+                    {item.children ? (
+                      <MenuDropdown
+                        title={item.title}
+                        path={item.path}
+                      >
+                        <ul className="bg-[#0f0f10] flex flex-col min-w-[15rem] border border-[#ffffff12] rounded-lg lg:py-2 text-white">
+                          {item.children.map(childItem => (
+                            <li key={childItem.path}>
+                              <Link className={`cursor-pointer hover:text-[#ff9900] px-4 py-2 w-full flex items-center`} href={childItem.path}>
+                                {childItem.title}
+                              </Link>
+                            </li>
+                          ))}
+                        </ul>
+                      </MenuDropdown>
+                    ) : (
+                      <Link href={item.path} className={`hover:text-[#ff9900] duration-300 ${pathname === item.path ? 'text-[#ff9900]' : ''}`}>
+                        {item.title}
+                      </Link>
+                    )}
+                  </li>
+                ))}
               </ul>
+              <div className="flex items-center gap-4">
+                <div className="max-lg:hidden lg:max-w-[120px] xl:max-w-[200px]">
+                  <BaseInput placeholder='Tìm kiếm...' startIcon={<SearchIcon />} />
+                </div>
+                <div className='cursor-pointer'>
+                  <ShoppingCartIcon color='white' />
+                </div>
+              </div>
             </div>
-          </AppSidebar>
+            {!isOpenSidebar && (
+              <div className="cursor-pointer lg:hidden p-2 hover:bg-[#ccc] hover:opacity-60 rounded-full duration-300" onClick={() => setIsOpenSidebar(true)}>
+                <MenuIcon fill="white" />
+              </div>
+            )}
+            <AppSidebar
+              open={isOpenSidebar}
+              setOpen={setIsOpenSidebar}
+              start="-24rem"
+              end="0"
+              exit="-24rem"
+            >
+              <div className="py-7">
+                <div>
+                  <BaseInput placeholder='Tìm kiếm...' endIcon={<SearchIcon />} />
+                </div>
+                <ul className="flex max-lg:flex-col ">
+                  <li className="flex px-3 py-1 hover:bg-[#0000000d] text-black">
+                    <Link href="/" className="p-3 w-full h-full">
+                      Trang chủ
+                    </Link>
+                  </li>
+                  <li className="flex px-3 py-1 hover:bg-[#0000000d] text-black">
+                    <Link href="/" className="p-3 w-full h-full">
+                      Giới thiệu
+                    </Link>
+                  </li>
+                  <li className="flex px-3 py-1 hover:bg-[#0000000d] text-black">
+                    <Link href="/" className="p-3 w-full h-full">
+                      Dịch vụ
+                    </Link>
+                  </li>
+                  <li className="flex px-3 py-1 hover:bg-[#0000000d] text-black">
+                    <Link href="/" className="p-3 w-full h-full">
+                      Kiến trúc
+                    </Link>
+                  </li>
+                  <li className="flex px-3 py-1 hover:bg-[#0000000d] text-black">
+                    <Link href="/" className="p-3 w-full h-full">
+                      Nội thất
+                    </Link>
+                  </li>
+                  <li className="flex px-3 py-1 hover:bg-[#0000000d] text-black">
+                    <Link href="/" className="p-3 w-full h-full">
+                      Tin tức
+                    </Link>
+                  </li>
+                  <li className="flex px-3 py-1 hover:bg-[#0000000d] text-black">
+                    <Link href="/" className="p-3 w-full h-full">
+                      Liên hệ
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </AppSidebar>
+          </div>
         </div>
       </div>
       <div className="h-[70px]" />
--- app/san-pham/page.tsx
+++ app/san-pham/page.tsx
@@ -11,18 +11,18 @@ async function Service({ searchParams }: { searchParams: { [key: string]: string
     <div className="max-w-6xl m-auto mb-4">
       <div className="py-10 flex flex-col">
         <div className="px-2 mb-4 flex justify-between">
-          <div className="flex">
+          <div className="flex items-center">
             <Link href="/">Trang chủ</Link>
             <span className="px-2">/</span>
             <p className="font-bold">Cửa hàng</p>
           </div>
-          <div className="max-w-[]">
+          <div>
             <Filter />
           </div>
         </div>
         <div className="flex flex-wrap w-full">
           {data.map(product => (
-            <div className="flex flex-col items-center max-w-[25%] basis-1/4 px-2 pb-4" key={product.id}>
+            <div className="flex flex-col items-center md:max-w-[25%] md:basis-1/4 px-2 pb-4" key={product.id}>
               <Link href={`/san-pham/${product.slug}`} className="overflow-hidden cursor-pointer">
                 <Image src={product.images[0].imageUrl} alt={product.name} width={100} height={100} className="w-full hover:scale-110 duration-500" />
               </Link>
